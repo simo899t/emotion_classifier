da4d7cb2688f70acc3e42d7c3e1473a59ebfd14f
Johannes Rothe <jorot24@student.sdu.dk>
2026-05-06 15:11:29 +0200

    Hanne Holm

--- testing.py
+++ testing.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import torch
 
 sns.set_theme(style="darkgrid")
+plt.style.use('seaborn-v0_8')
 
 def _to_df(history: torch.Tensor, name: str) -> pd.DataFrame:
     df = pd.DataFrame(history.numpy(), columns=["epoch", "loss", "accuracy"])
@@ -15,7 +16,19 @@ def _to_df(history: torch.Tensor, name: str) -> pd.DataFrame:
 
 
 def plot(T1, T2):
-    # TODO
+    f, (ax1, ax2) = plt.subplots(1, 2) 
+    ax1.plot(T1[0], T1[1], label='T1')
+    ax1.plot(T1[0], T2[1], label='T2')
+    ax1.set_title('Loss')
+    ax1.legend()
+
+    ax2.plot(T2[0], T1[2], label='T1')
+    ax2.plot(T2[0], T2[2], label='T2')
+    ax2.set_title('Acc')
+    ax2.legend()
+
+
+    plt.tight_layout()
     plt.show()
 
 
@@ -34,15 +47,15 @@ if __name__ == "__main__":
     HIDDEN_DIM = 64
     NUM_LAYERS = 2
     LEARNING_RATE = 1e-2
-    EPOCHS = 2
+    EPOCHS = 200
 
-    model_1 = m1.get_model(vocab, EMBED_DIM, HIDDEN_DIM,NUM_LAYERS)
+    model_1 = m1.get_model(vocab, EMBED_DIM, HIDDEN_DIM, NUM_LAYERS)
     print(f"\n--- Training TextRNN with lr: {LEARNING_RATE} on {EPOCHS} epocs ---")
     T1 = m1.train(model_1, encoded_train_corpus, lengths, targets, use_lengths=True, epochs=EPOCHS, lr=LEARNING_RATE, log_interval=1)
 
-    model_2 = m1.get_model(vocab, EMBED_DIM, HIDDEN_DIM,NUM_LAYERS)
+    model_2 = m1.get_model(vocab, EMBED_DIM, HIDDEN_DIM, NUM_LAYERS)
     print(f"\n--- Training TextRNN with lr: {LEARNING_RATE} on {EPOCHS} epocs ---")
-    T2 = m1.train(model_1, encoded_train_corpus, lengths, targets, use_lengths=True, epochs=EPOCHS, lr=LEARNING_RATE, log_interval=1)
+    T2 = m1.train(model_2, encoded_train_corpus, lengths, targets, use_lengths=True, epochs=EPOCHS, lr=LEARNING_RATE, log_interval=1)
 
     plot(T1, T2)
 
